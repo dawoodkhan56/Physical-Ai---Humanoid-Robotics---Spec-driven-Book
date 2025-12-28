a61f90fcf27bd643658ddbe767e224c74ebf25d4
dawood khan <dk5519266@gmail.com>
2025-12-29 01:21:50 +0500

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -7,7 +7,7 @@ const config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://physical-ai-humanoid-guide.netlify.app/', // Placeholder URL
+  url: 'https://physical-ai-humanoid-robotics-spec.vercel.app/', // Placeholder URL
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
